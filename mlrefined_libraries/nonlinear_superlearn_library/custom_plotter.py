6b80e29570dbd1539905db559524aeec8eb94e16
Jeremy Watt <jermwatt@gmail.com>
2017-11-01 03:14:12 -0500

    multilayer perceptron updates

--- mlrefined_libraries/nonlinear_superlearn_library/custom_plotter.py
+++ mlrefined_libraries/nonlinear_superlearn_library/custom_plotter.py
@@ -60,25 +60,41 @@ class Visualizer:
         plt.show()
     
     # compare regression cost histories from multiple runs
-    def compare_regression_histories(self,histories,start):
+    def compare_regression_histories(self,histories,start,**kwargs):
         # initialize figure
-        fig = plt.figure(figsize = (9,3))
+        fig = plt.figure(figsize = (8,3))
 
         # create subplot with 1 panel
         gs = gridspec.GridSpec(1, 1) 
         ax = plt.subplot(gs[0]); 
+        
+        # any labels to add?        
+        labels = [' ',' ']
+        if 'labels' in kwargs:
+            labels = kwargs['labels']
 
         # run through input histories, plotting each beginning at 'start' iteration
-        c = 1   # paramter controls width of cost function plot, so if we get overlapping histories we can still see both visually
-        for history in histories:
-            ax.plot(np.arange(start,len(history),1),history[start:],linewidth = 4*(0.8)**(c))
-            c += 1
+        for c in range(len(histories)):
+            history = histories[c]
+            
+            label = 0
+            if c == 0:
+                label = labels[0]
+            else:
+                label = labels[1]
+                
+            # check if a label exists, if so add it to the plot
+            if np.size(label) == 0:
+                ax.plot(np.arange(start,len(history),1),history[start:],linewidth = 3*(0.8)**(c)) 
+            else:               
+                ax.plot(np.arange(start,len(history),1),history[start:],linewidth = 3*(0.8)**(c),label = label) 
 
         # clean up panel
         ax.set_xlabel('iteration',fontsize = 12)
         ax.set_ylabel('cost function value',fontsize = 12)
         ax.set_title('cost function value at each step of gradient descent',fontsize = 15)
-        #plt.legend(loc='upper right')
+        if np.size(label) == 0:
+            plt.legend(loc='upper right')
         ax.set_xlim([start - 1,len(history)+1])
         plt.show()
         
@@ -195,7 +211,6 @@ class Visualizer:
                 ax2.contourf(s,t,z+1,colors = custom_colors[1],levels = [0,1],zorder = 1,alpha = 0.1)
         
         plt.show()
-    
     
     # compare regression cost histories from multiple runs
     def compare_classification_histories(self,count_histories,cost_histories,start,**kwargs):
